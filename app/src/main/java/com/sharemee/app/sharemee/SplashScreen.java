79f6a777d01ac1ce296f02d92400b3cf7c06ca46
Louis <luis.omoreau@gmail.com>
2015-02-12 18:21:24 +0100

    initial commit

---
+++ app/src/main/java/com/sharemee/app/sharemee/SplashScreen.java
@@ -0,0 +1,54 @@
+package com.sharemee.app.sharemee;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class SplashScreen extends Activity {
+
+    /** Durée d'affichage du SplashScreen */
+    protected int _splashTime = 5000;
+
+    private Thread splashTread;
+
+    /** Chargement de l'Activity */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        final SplashScreen sPlashScreen = this;
+
+        /** Thread pour l'affichage du SplashScreen */
+        splashTread = new Thread()
+        {
+            @Override
+            public void run()
+            {
+                try
+                {
+                    synchronized(this)
+                    {
+                        wait(_splashTime);
+                    }
+                } catch(InterruptedException e) {}
+                finally
+                {
+                    finish();
+                    Intent i = new Intent();
+                    i.setClass(sPlashScreen, MainActivity.class);
+                    startActivity(i);
+                }
+            }
+        };
+
+        splashTread.start();
+    }
+
+}
