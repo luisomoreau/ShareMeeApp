4f12a0751dfae199bc35c9497ca53fe2b8f7d7f6
Marin <marinbernaert@aol.com>
2015-04-27 15:07:09 +0200

    ConnectionConfig setup

---
+++ app/src/main/java/com/sharemee/app/sharemee/util/ConnectionConfig.java
@@ -0,0 +1,17 @@
+package com.sharemee.app.sharemee.util;
+
+/**
+ * Created by Marin on 27/04/2015.
+ */
+public class ConnectionConfig {
+
+
+    public String baseURL="http://192.168.1.34/sharemee/";
+    //public String baseURL="http://sharemee.com/";
+    //public String baseURL="http://10.0.2.2/sharemee/";
+
+
+    public String getBaseURL() {
+        return baseURL;
+    }
+}
