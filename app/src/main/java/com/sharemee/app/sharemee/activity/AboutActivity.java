ddd9beab8cefa42f074a70fcf8ca72e0f11c62d6
Louis <luis.omoreau@gmail.com>
2015-05-11 21:11:52 +0200

    added layout for search activity

--- app/src/main/java/com/sharemee/app/sharemee/activity/AboutActivity.java
+++ app/src/main/java/com/sharemee/app/sharemee/activity/AboutActivity.java
@@ -2,8 +2,11 @@ package com.sharemee.app.sharemee.activity;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
 
 import com.sharemee.app.sharemee.R;
 
@@ -12,7 +15,13 @@ public class AboutActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about);
+        // don’t set any content view here, since its already set in BaseActivity
+        FrameLayout frameLayout = (FrameLayout)findViewById(R.id.activity_frame);
+        // inflate the custom activity layout
+        LayoutInflater layoutInflater = (LayoutInflater)getSystemService(LAYOUT_INFLATER_SERVICE);
+        View activityView = layoutInflater.inflate(R.layout.activity_about, null,false);
+        // add the custom layout of this activity to frame layout.
+        frameLayout.addView(activityView);
     }
 
 
--- app/src/main/java/com/sharemee/app/sharemee/activity/SearchableActivity.java
+++ app/src/main/java/com/sharemee/app/sharemee/activity/SearchableActivity.java
@@ -8,9 +8,12 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
 
 import com.sharemee.app.sharemee.R;
 
@@ -19,8 +22,13 @@ public class SearchableActivity extends BaseActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_searchable);
-        handleIntent(getIntent());
+        // don’t set any content view here, since its already set in BaseActivity
+        FrameLayout frameLayout = (FrameLayout)findViewById(R.id.activity_frame);
+        // inflate the custom activity layout
+        LayoutInflater layoutInflater = (LayoutInflater)getSystemService(LAYOUT_INFLATER_SERVICE);
+        View activityView = layoutInflater.inflate(R.layout.activity_searchable, null,false);
+        // add the custom layout of this activity to frame layout.
+        frameLayout.addView(activityView);
     }
 
     @Override
